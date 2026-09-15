1891de99ae63e75f5ce666a6bad02a5cfc90a7af
StDarlex <stdarlex.marketing@gmail.com>
2026-09-15 13:58:13 -0600

    Corregir procesamiento de solicitudes JSON

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 /* =========================================================
    CONFIGURACIÓN GENERAL
 ========================================================= */
